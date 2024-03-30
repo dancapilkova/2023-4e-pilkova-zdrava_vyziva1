9b74bcbdb4ece044971c6c82690df88b526e8b84
dancapilkova <daniela.pilkova@student.gyarab.cz>
2024-03-30 21:05:29 +0100

    posledni

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import NewRecipe, Recipes, Detail
+from .views import NewRecipe, Recipes, Detail, Delete, Edit
 
 urlpatterns = [
     path("new/", NewRecipe.as_view(), name="new_recipe"),
     path("", Recipes.as_view(), name="recipes"),
     path("<slug:pk>/", Detail.as_view(), name="detail"),
+    path("delete/<slug:pk>/", Delete.as_view(), name="delete"),
+     path("edit/<slug:pk>/", Edit.as_view(), name="edit"),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,27 @@
-from django.views.generic import CreateView, ListView, DetailView
+from django.views.generic import CreateView, ListView, DetailView, DeleteView, UpdateView
 from .models import Recipe
 from .forms import NewRecipeForm
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.db.models import Q
 
 
 class Recipes(ListView):
     template_name = "recipes/recipes.html"
     model = Recipe
     context_object_name = "recipes"
+     
+    def get_queryset(self, **kwargs):
+        q = self.request.GET.get("query")
+        if q:
+            recipes = self.model.objects.filter(
+                Q(title__icontains=q)
+                | Q(instructions__icontains=q)
+                | Q(cuisine_types__icontains=q)
+                | Q(meal_type__icontains=q)
+            )
+        else:
+            recipes = self.model.objects.all()
+        return recipes
 
 class Detail(DetailView):
     template_name = "recipes/detail.html"
@@ -24,3 +38,18 @@ class NewRecipe(LoginRequiredMixin, CreateView):
         form.instance.user = self.request.user
         return super(NewRecipe, self).form_valid(form)
 
+class Delete(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Recipe
+    success_url = "/recipes/"
+
+    def test_func(self):
+        return self.request.user == self.get_object().user
+    
+class Edit(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    template_name = "recipes/edit.html"
+    model = Recipe
+    form_class = NewRecipeForm
+    success_url = "/recipes/"
+
+    def test_func(self):
+        return self.request.user == self.get_object().user
